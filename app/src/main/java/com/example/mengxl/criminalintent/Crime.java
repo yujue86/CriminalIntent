f94fb186c90faa2accc96e96995b64777e14181b
yujue86 <mengxl2020@gmail.com>
2015-11-07 00:48:40 +0800

    chapter 8 update 1

--- app/src/main/java/com/example/mengxl/criminalintent/Crime.java
+++ app/src/main/java/com/example/mengxl/criminalintent/Crime.java
@@ -1,6 +1,7 @@
 package com.example.mengxl.criminalintent;
 
 import java.util.UUID;
+import java.util.Date;
 
 /**
  * Created by mengxl on 15/11/4.
@@ -9,9 +10,12 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -24,5 +28,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
