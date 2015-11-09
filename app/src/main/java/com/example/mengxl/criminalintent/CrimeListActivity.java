5bcb51dd538cf2aae3e5e4d8001ae33a9e7c953c
yujue86 <mengxl2020@gmail.com>
2015-11-09 23:27:34 +0800

    chapter 9 update 1
    ListFragment

--- app/src/main/java/com/example/mengxl/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/mengxl/criminalintent/CrimeListActivity.java
@@ -2,11 +2,13 @@ package com.example.mengxl.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity{
+/**
+ * Created by mengxl on 15/11/9.
+ */
+public class CrimeListActivity extends SingleFragmentActivity{
 
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
     }
-
 }
---
+++ app/src/main/java/com/example/mengxl/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.example.mengxl.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by mengxl on 15/11/9.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null){
+            fragment  = createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer,fragment).commit();
+        }
+
+    }
+
+}
